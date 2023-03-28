a38eac32c8688b041dd33e1ac9da326c5d50a13a
iamhytrax <manojhv13@gmail.com>
2023-03-28 16:21:33 +0530

    commit

--- variables.java
+++ variables.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class variables {
     public static void main(String[]args){
       Scanner sc = new Scanner(System.in);
+      
+      //inputs
+      String c = sc.next();
+      System.out.println(c);
+
+      //inputs
       String a = sc.nextLine();
-      System.out.println(a+" the output name");
-  
-  
-  
-  
-  
-  
-  
+      System.out.println(a + " the output name");
+
+
+      //inputs 
+      boolean tf = sc.nextBoolean();
+      System.out.print(tf);
+
       }
 
 }
