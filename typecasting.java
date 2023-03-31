f7956ececb297b5b27c4b112cd9e263a80c31392
iamhytrax <manojhv13@gmail.com>
2023-03-31 08:40:28 +0530

    commit

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+      double a = 9.89;
+      int b = (int)a; // Automatic casting: int to double
+  
+      System.out.println(a);      // Outputs 9
+      System.out.println(b);   // Outputs 9.0
+    }
+  }
